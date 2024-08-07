a967437c4c5eca2d099a4b82fdbf530631b80e8b
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-08-07 12:37:13 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from places.models import Place
+from places.models import Place, Image
 
 admin.site.register(Place)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('order', 'place')
+    
--- places/models.py
+++ places/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=255,
-                             verbose_name='Название места')
+                             verbose_name='Название компании')
     description_short = models.TextField(verbose_name='Краткое описание',
                                          blank=True,
                                          null=True)
@@ -17,3 +17,16 @@ class Place(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place,
+                              on_delete=models.CASCADE,
+                             verbose_name='Место',
+                             related_name='images')
+    img = models.ImageField(verbose_name='Фото')
+    order = models.PositiveIntegerField(verbose_name='Порядковый номер',
+                                        default=0)
+    
+    def __str__(self):
+        return self.place.title
