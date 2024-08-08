25b2f06045b0b1ef34c8a686b6e7cb752d1e549b
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-08-08 20:14:17 +0300

    get image preview

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from places.models import Place, Image
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 
 class PlaceImageInline(admin.TabularInline):
     model = Image
+    fields = ('img', 'get_preview')
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        url = obj.img.url
+        width = 200
+        height = 150
+
+        return format_html(
+           '{}',
+           mark_safe(f'<img src="{url}" width="{width}" height={height} />')
+           )
 
 
 @admin.register(Place)
