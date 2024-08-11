3130cf732af2a82eacae37bb9366c7eaadaf2245
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-08-11 11:58:25 +0300

    Change verbose name to title

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=255,
-                             verbose_name='Название компании')
+                             verbose_name='Название')
     description_short = models.TextField(verbose_name='Краткое описание',
                                          blank=True,
                                          null=True)
@@ -15,7 +15,6 @@ class Place(models.Model):
                             null=True)
     lon = models.FloatField(verbose_name='Долгота',
                             null=True)
-    
 
     def __str__(self):
         return self.title
@@ -29,11 +28,9 @@ class Image(models.Model):
     img = models.ImageField(verbose_name='Фото')
     order = models.PositiveIntegerField(verbose_name='Порядковый номер',
                                         default=0)
-    
 
     def __str__(self):
         return self.place.title
-    
 
     class Meta:
         ordering = ['order']
